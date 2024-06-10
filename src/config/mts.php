f91b8f4fd3c05f3970c149a999d4c3f71df7fab7
shigabutdinoff <shigabutdinovbr@gmail.com>
2024-06-11 00:44:32 +0300

    Move files from the "config" folder to the root folder. Add the necessary strokes to the .env file. Run the command to clear and cache the configuration. Example of sending a message to a Telegram chat.

---
+++ src/config/mts.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'url' => env('MTS_URL'),
+    'token' => env('MTS_TOKEN'),
+    'login' => env('MTS_LOGIN'),
+    'password' => env('MTS_PASSWORD'),
+    'naming' => env('MTS_NAMING'),
+    'phone' => env('MTS_PHONE'),
+);
---
+++ src/config/telegram.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'url' => env('TG_URL'),
+    'token' => env('TG_TOKEN'),
+    'chat_id' => env('TG_CHAT_ID')
+);
